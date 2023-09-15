4130321ab2c05af5ab7a9afe2e8522908ef85a14
JuanNilo <juan.nilo@alumnos.ucn.cl>
2023-09-15 19:55:23 -0300

    Add Key

--- app/components/componentes-home/Categorias-Selector.jsx
+++ app/components/componentes-home/Categorias-Selector.jsx
@@ -4,7 +4,7 @@ export default function CategoriasSelectorMenu(){
     return(
         <div className="gap-4  w-[100%] h-[100px]">
             {categorias.map(categoria => (
-                <div className="m-0 w-[100%] hover:bg-zinc-300">
+                <div key={categoria} className="m-0 w-[100%] hover:bg-zinc-300">
                     {categoria}
                 </div>
             ))}
--- app/components/componentes-home/Sucursal-Selector.jsx
+++ app/components/componentes-home/Sucursal-Selector.jsx
@@ -7,7 +7,7 @@ export default function SucursalSelctorMenu () {
     return(
         <div className="gap-4  w-[100%] h-[100px]">
             {categorias.map(categoria => (
-                <div className="m-0 w-[100%] hover:bg-zinc-300">
+                <div key={categoria} className="m-0 w-[100%] hover:bg-zinc-300">
                     {categoria}
                 </div>
             ))}
